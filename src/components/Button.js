9d758694bfbc54a6b7d34c5d6825f5624a7e1644
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2022-03-21 14:15:08 +0330

    on click event

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelBlue",
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
