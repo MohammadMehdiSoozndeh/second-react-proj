4d658fa036f421d75559171c44aa8531f14e45d7
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2022-03-22 10:11:19 +0430

    add task

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.ceil(Math.random() * 100000);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -48,7 +55,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={togggleReminder} />
       ) : (
